7793512ce5f2a50759282f8aebc15cfcf803b066
jluis12369 <luiz9999jose@gmail.com>
2023-06-25 22:37:23 -0400

    commit inicial

---
+++ src/routes/ceps.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express';
+import { CepController } from './../controllers/CepController';
+
+export const cepsRouter = Router();
+const cepController = new CepController();
+
+cepsRouter.post('/', (req, res) => cepController.save(req, res));
+cepsRouter.get('/busca/cep/:cep', (req, res) => cepController.findByCep(req, res));
+cepsRouter.get('/busca/logradouro/:logradouro', (req, res) => cepController.findByLogradouro(req, res));
+cepsRouter.delete('/', (req, res) => cepController.truncate(req, res))
